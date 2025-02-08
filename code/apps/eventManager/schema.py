d54e6ef8426de9831add0eb3cdd88a5cd0ece95e
ninimefarishvili <164371339+ninimefarishvili@users.noreply.github.com>
2025-02-08 10:41:11 -0800

    Update schema.py

--- code/apps/eventManager/schema.py
+++ code/apps/eventManager/schema.py
@@ -1,6 +1,6 @@
 
 from ninja import Schema, Field, ModelSchema
-from .models import Stadium, Event
+from .models import Stadium, Event ,Ticket, OrganizerCompany
 
 class StadiumSchema(Schema):
     name: str
@@ -11,11 +11,30 @@ class StadiumSchema(Schema):
         model = Stadium
         exclude = ["id"]
 
-class EventSchema(ModelSchema):
+class EventSchema(Schema):
     name: str
     stadium: StadiumSchema
     is_active: bool
 
     class Meta:
         model = Event
-        exclude = ["date"]
+        exclude = ["id"]
+
+
+class TicketSchema(ModelSchema):
+    customer : str
+    event : EventSchema
+    bougtht_at : str
+
+    class Meta:
+        model = Ticket
+        exclude = ["id"]
+
+class OrganizerCompanySchema(ModelSchema):
+    name:str
+    address :  str
+    events_organized : EventSchema
+
+    class Meta:
+        model = OrganizerCompany
+        exclude = ['id']    
