2d3a401833688f7505819bc3e309c219fb39780b
ninimefarishvili <164371339+ninimefarishvili@users.noreply.github.com>
2025-03-01 10:03:45 -0800

    Update views.py

--- code/apps/eventManager/views.py
+++ code/apps/eventManager/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Stadium
+
+
+class StadiumList(APIView):
+    def get(self, request, format=None):
+        stadiums = Stadium.objects.all()  
+        return Response(stadiums)
